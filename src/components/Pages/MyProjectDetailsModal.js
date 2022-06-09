328ef4b63cb19901299aedf58bf0fe9f33f3e2e3
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-06-09 21:49:56 +0600

    Project More Image Added

--- src/components/Pages/MyProjectDetailsModal.js
+++ src/components/Pages/MyProjectDetailsModal.js
@@ -9,13 +9,17 @@ const MyProjectDetailsModal = ({ modalProjectData }) => {
             {/* border-4 border-red-600  */}
             <div class="modal">
                 <div class="modal-box w-8/12 max-w-4xl">
-                <label for="my-modal-6" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <label for="my-modal-6" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
 
                     <h3 className="font-bold text-lg"> {modalProjectData?.name} </h3>
-                    <img className='w-2/6' src={modalProjectData?.allImg[0]} alt="" />
-
-                    <img className='w-2/6' src={modalProjectData?.allImg[2]} alt="" />
-                    <img className='w-2/6' src={modalProjectData?.img1} alt="" />
+                    <div className='grid grid-cols-2'>
+                        {
+                            modalProjectData?.allImg.map(arryImg => (
+                                <img className=' ' src={arryImg} alt="" />
+                            ))
+                        }
+                    </div>
+                    <img src={modalProjectData?.img1} alt="" />
                     <p className="py-4">
                         <p>{modalProjectData?.name}</p>
                     </p>
